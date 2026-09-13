0da9723666c1ebed650d114cbbd13463c20aea95
Viviane <vivianefrancodasilva@gmail.com>
2026-09-12 21:23:14 -0300

    Fix: corrigir path do base para documind_ai

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/documind-ai/',
+  base: '/documind_ai/',
   server: {
     host: "0.0.0.0",
     port: 3000,
